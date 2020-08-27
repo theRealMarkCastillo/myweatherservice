be42cf2b0cad597ab078b268433e19b734df8494
Mark Castillo <1103472+theRealMarkCastillo@users.noreply.github.com>
2020-08-27 11:01:44 -0700

    add prometheus metrics

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Prometheus;
 
 namespace myweatherservice
 {
@@ -39,9 +40,12 @@ namespace myweatherservice
 
             app.UseAuthorization();
 
+            app.UseMetricServer();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapMetrics();
             });
         }
     }
